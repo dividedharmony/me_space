56d4e09a010e53f5e4ea45b0e30b5bd0f4713a7a
David Harmon <dharmon202@gmail.com>
2021-04-03 19:31:26 -0400

    Remove Nuxt-specific content
    These elements and styles do not carry over into our vision for
    the site.

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,7 @@
 <template>
   <Container>
     <div>
-      <Logo />
       <Headline>me_space</Headline>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </Container>
 </template>
@@ -30,15 +11,5 @@ export default {}
 </script>
 
 <style>
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+/* TODO add page-specific styling */
 </style>
