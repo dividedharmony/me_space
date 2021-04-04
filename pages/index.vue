bdfda8b17a4530c23f0ef707a7436a2e1e96630b
David Harmon <dharmon202@gmail.com>
2021-04-03 20:26:46 -0400

    Add David Harmon bio to home page
    This content may be edited in the future but it is basically
    what I want on the home page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,51 @@
 <template>
   <Container>
-    <div>
-      <Headline>me_space</Headline>
+    <div class="intro">
+      <Headline>David Harmon</Headline>
+    </div>
+    <div class="intro-text">
+      <p>
+        David Harmon [he/him] is a writer and fullstack developer born and bred
+        in the rolling hills of North Carolina. His hobbies include painting
+        way-too-many-miniatures, sketching maps, and tending his succulents and
+        indoor herb garden. He enjoys reading manga and popular physics books,
+        sipping coffee, and taking a good nap. His bad-habits include buying
+        more books than he has time to read, more games than he has time to
+        play, and more snacks than he can eat. He enjoys pouring his lifeblood
+        into Dark Souls, then recuperating with Cookie Clicker and the
+        peaceful-mode of Minecraft. He dabbles in digital-based artwork and the
+        endless depths of Magic: The Gathering. You can follow his escapades on
+        Twitter <a href="https://twitter.com/dharmon202">@dharmon202</a>
+      </p>
+
+      <p>
+        David is a new writer in the Dungeons and Dragons universe, but has
+        already accrued several titles on the DMsGuild. His AL-legal titles
+        include “CCC-SEA-01-02 Oh, Not the Bees!”, “DC-PoA-HARM-01-01 First Time
+        for Everything”, and “DC-PoA-HARM-01-02 Time Flies When You’re Delving
+        Dungeons”. He contributed to the Halloween-favorite, “BEHOLD! Liar’s
+        Night”, and wrote a spooktacular adventure for the winter-time favorite,
+        “12 Days of Midwinter”. He also contributed to the 5e anthology that
+        redefined Dunwood for 5e: “Dunwood - Demons, Druids, &amp; Danger”.
+      </p>
+
+      <p>
+        David is a master of over-the-top NPC portrayals at Epic events,
+        including The Black Viper, Volo, and Pipyap. Having captured well over
+        50 mortal souls via Infernal contracts, David is a rising star among the
+        Infernal ranks and a favorite of Asmodeus. As a convention organizer for
+        Triangle Adventurers, David has organized multiple conventions for Extra
+        Life, raising over $5,000 for Duke Children’s Hospital. He has also
+        organized and contributed to online events, such as XugCon II and Fall
+        in Love with DnD II, promoting a sense of community and shared
+        experiences throughout the COVID-19 pandemic. As a perma-DM for
+        Adventurers League, David strives to showcase the inspired and diverse
+        voices who write adventures for the AL-community. He is a master at
+        creating online and in-person games where strangers can come together to
+        learn dnd in a safe, respectful, and caring environment. David excels at
+        weaving stories that are entirely personal to each player and creating
+        lasting memories that have forged countless friendships.
+      </p>
     </div>
   </Container>
 </template>
@@ -11,5 +55,7 @@ export default {}
 </script>
 
 <style>
-/* TODO add page-specific styling */
+.intro-text {
+  font-size: 20px;
+}
 </style>
