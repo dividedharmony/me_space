09c9d4740d089a59fd4b419979032f43b2be152f
David Harmon <dharmon202@gmail.com>
2021-04-03 23:20:04 -0400

    Add pages/_.vue for dynamically routing to CMS pages
    When a user navigates to a page not otherwise handled by static
    routes, this page will attempt to find a document in the nuxt
    content directory at the same relative path. If found, it renders
    the document; if not, it returns a 404 status.
    
    Later commits will style and improve the visuals of this page.

---
+++ pages/_.vue
@@ -0,0 +1,31 @@
+<template>
+  <Container>
+    <div v-if="!!article" class="above-fold">
+      <Headline :size="2">{{ article.title }}</Headline>
+    </div>
+    <div class="below-fold">
+      <nuxt-content :document="article" />
+    </div>
+  </Container>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    try {
+      const article = await $content(`articles/${params.pathMatch}`).fetch()
+      return {
+        article,
+      }
+    } catch (e) {
+      console.error(e)
+      error({ statusCode: 404, message: 'Page not found!' })
+    }
+  },
+  data() {
+    return {
+      article: null,
+    }
+  },
+}
+</script>
