4cd4ede39f3dcee989ebad56facbd76a6ba1a67c
David Harmon <dharmon202@gmail.com>
2021-04-03 22:10:31 -0400

    Add links to David Harmon's social media pages

--- components/Profile.vue
+++ components/Profile.vue
@@ -5,6 +5,30 @@
       src="~/assets/images/me.jpeg"
       alt="David Harmon"
     />
+    <ul class="social-media-links">
+      <li class="social-media">
+        Github:
+        <a href="https://github.com/dividedharmony/" class="link"
+          >@dividedharmony</a
+        >
+      </li>
+      <li class="social-media">
+        Twitter:
+        <a href="https://twitter.com/dharmon202" class="link">@dharmon202</a>
+      </li>
+      <li class="social-media">
+        Facebook:
+        <a href="https://www.facebook.com/david.the.harmon" class="link"
+          >@David Harmon</a
+        >
+      </li>
+      <li class="social-media">
+        LinkedIn:
+        <a href="https://www.linkedin.com/in/harmond/" class="link"
+          >/in/harmond</a
+        >
+      </li>
+    </ul>
   </aside>
 </template>
 
@@ -18,5 +42,19 @@
 .profile-image {
   height: 200px;
   width: 200px;
+}
+
+.social-media-links {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+.link {
+  color: rgb(51, 51, 51);
+
+  &:hover {
+    color: rgb(63, 63, 63);
+  }
 }
 </style>
